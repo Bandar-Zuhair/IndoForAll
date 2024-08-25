58a53a1622e3a1659f87ed70d90a99e814ab96e6
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-08-26 03:14:00 +0700

    Update indoforall.js

--- indoforall.js
+++ indoforall.js
@@ -261,35 +261,35 @@ let indoforall_adsVideosArray = [
 /* Array For Clint Rate Data */
 let indoforall_clintRateData = [
     {
-        date: '2024/7/31',
+        date: '2024/8/25',
         profileSRC: 'تقييم-عملائنا/man.svg',
         name: 'راكان',
         comment: 'تم الاستقدام من اندو للجميع وما شاء الله سرعة في الانجاز وسرعة في الرد على كل الاستفسارات',
         starSRC: 'تقييم-عملائنا/stars5.svg',
     },
     {
-        date: '2024/7/27',
+        date: '2024/8/19',
         profileSRC: 'تقييم-عملائنا/woman.svg',
         name: 'ام بندر',
         comment: 'الحمد لله وصلت العاملة والله يجعلها عون لنا ويجزاكم كل خير',
         starSRC: 'تقييم-عملائنا/stars5.svg',
     },
     {
-        date: '2024/7/12',
+        date: '2024/8/04',
         profileSRC: 'تقييم-عملائنا/man.svg',
         name: 'ابو خالد',
         comment: 'الى الان هذا افضل موقع طلبت منه استقدام عمالة اندونيسية',
         starSRC: 'تقييم-عملائنا/stars5.svg',
     },
     {
-        date: '2024/7/03',
+        date: '2024/7/26',
         profileSRC: 'تقييم-عملائنا/woman.svg',
         name: 'عبير صالح',
         comment: 'العاملة المنزلية ممتازة واتمنى تضيفو عاملة فنادق كمان',
         starSRC: 'تقييم-عملائنا/stars4.svg',
     },
     {
-        date: '2024/6/28',
+        date: '2024/7/13',
         profileSRC: 'تقييم-عملائنا/man.svg',
         name: 'عامر',
         comment: 'رجل صادق انصح التعامل معه',
